ce4cdb45ae12c69ef45f729d1e5396495e48bf13
Justin Cordova <justinavodroc@gmail.com>
2026-02-12 23:22:03 -0500

    feat: load park images with Glide

--- app/src/main/java/com/codepath/nationalparks/NationalParksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/nationalparks/NationalParksRecyclerViewAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.codepath.nationalparks.R.id
 
 /**
@@ -28,11 +29,10 @@ class NationalParksRecyclerViewAdapter(
     inner class ParkViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
         var mItem: NationalPark? = null
 
-        // TODO: Step 4a - Add references for remaining views from XML
-        val mParkName: TextView = mView.findViewById(id.park_name) as TextView
-        val mParkLocation: TextView = mView.findViewById(id.park_location) as TextView
-        val mParkDescription: TextView = mView.findViewById(id.park_description) as TextView
-        val mParkImage: ImageView = mView.findViewById(id.park_image) as ImageView
+        val mParkName: TextView = mView.findViewById(id.park_name)
+        val mParkLocation: TextView = mView.findViewById(id.park_location)
+        val mParkDescription: TextView = mView.findViewById(id.park_description)
+        val mParkImage: ImageView = mView.findViewById(id.park_image)
 
         override fun toString(): String {
             return mParkName.toString() + " '" + mParkDescription.text + "'"
@@ -42,13 +42,16 @@ class NationalParksRecyclerViewAdapter(
     override fun onBindViewHolder(holder: ParkViewHolder, position: Int) {
         val park = parks[position]
 
-        // TODO: Step 4b - Bind the park data to the views
         holder.mItem = park
         holder.mParkName.text = park.name
         holder.mParkLocation.text = park.location
         holder.mParkDescription.text = park.description
 
-        // TODO: Step 4c - Use Glide to load the first image
+        val imageUrl = park.imageUrl
+        Glide.with(holder.mView)
+            .load(imageUrl)
+            .centerInside()
+            .into(holder.mParkImage)
 
 
         // Sets up click listener for this park item
